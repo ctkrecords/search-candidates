a30cd44ffea20c80ec0401e3af4c57713c1f3797
ctkrecords <ctkrecordings@gmail.com>
2019-12-17 17:17:40 -0600

    Redirect logged user to the candidates path

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 Rails.application.routes.draw do
+  authenticated :user do
+    root to: "candidates#index"
+  end
+  
+  unauthenticated :user do
+    root "pages#home"
+  end
   get 'pages/home'
   devise_for :users, path: '', path_names: {sign_in: 'login', sign_out: 'logout'}, :skip => [:registrations, :password]
   resources :candidates
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: 'pages#home'
+
 end
